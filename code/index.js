1165cbb654c168754996b707438d39d9c1893179
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-02-26 19:45:33 +0100

    Add second npm script

--- code/index.js
+++ code/index.js
@@ -1,6 +1,12 @@
-const _ =  require("lodash");
+import _ from "lodash";
+import moment from "moment";
 const numbers = [1,4,43,21,74,32,15];
 
 _.each(numbers, (numbers) => {
     console.log(numbers * 2)
 });
+
+let firstData = new Date();
+console.log(firstData);
+let newData = moment("02-26-2023 19:43").format();
+console.log(newData);
