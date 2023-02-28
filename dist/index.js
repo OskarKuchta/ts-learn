a50d982bbb1765329cbd7586a8c4054bedcb6562
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-02-28 17:40:33 +0100

    Configuration typescript and add npm modules

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+var user = { name: "Oskar", age: 24 };
+console.log(user.name);
+console.log(user);
+let num = 123;
+console.log(num);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+var user = { name: "Oskar", age: 24 };
+console.log(user.name);
+console.log(user);
+let num: number = 123;
+console.log(num);
