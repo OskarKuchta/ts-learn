cc9b7ba5588dcba5e145e7e6fe451376d4702d4c
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-02-28 18:37:47 +0100

    Add some changes and fix quokka

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,7 @@ console.log(user.name);
 console.log(user);
 let num = 123;
 console.log(num);
-//# sourceMappingURL=index.js.map
+
+const firstInput = document.querySelector("#input1");
+const secondInput = document.querySelector("#input2");
+const btn = document.querySelector("button");
