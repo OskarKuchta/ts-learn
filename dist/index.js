64b4f6e5b26e892bbd8aea5aa5b65779a002734e
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-02-28 22:15:57 +0100

    All day spend to fix quokka, finally done :D

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 const firstInput = document.querySelector("#input1");
 const secondInput = document.querySelector("#input2");
 const addBtn = document.querySelector("button");
-const add = (v1, v2) => v1 + v2;
+const add = (v1, v2) => {return v1 + v2};
 addBtn.addEventListener("click", () => {
     const sum = add(firstInput.value, secondInput.value);
     console.log(sum);
-})
+});
