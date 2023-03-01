111d55b2f39f35e275b0573098c531df801b2234
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-03-01 14:01:36 +0100

    Different between JS and TS, simple example

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 const firstInput = document.querySelector("#input1");
 const secondInput = document.querySelector("#input2");
 const addBtn = document.querySelector("button");
-const add = (v1, v2) => {return v1 + v2};
+const add = (v1, v2) => {
+    return v1 + v2;
+};
 addBtn.addEventListener("click", () => {
-    const sum = add(firstInput.value, secondInput.value);
+    const sum = add(Number(firstInput.value), Number(secondInput.value));
     console.log(sum);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-var user = { name: "Oskar", age: 24 };
-console.log(user.name);
-console.log(user);
-let num: number = 123;
-console.log(num);
+const firstInput: HTMLInputElement = document.querySelector("#input1");
+const secondInput: HTMLInputElement = document.querySelector("#input2");
+const addBtn = document.querySelector("button");
+const add = (v1: number, v2: number) => {
+    return v1 + v2
+};
+addBtn.addEventListener("click", () => {
+    const sum = add(Number(firstInput.value), Number(secondInput.value));
+    console.log(sum);
+});
