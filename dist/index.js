d51aecb5b76738a2a7747683d929b64eef06b612
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-03-05 16:57:51 +0100

    Some tsconfig + examples of using typescript

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,31 @@
-console.log(typeof 12354654);
+var Sides;
+(function (Sides) {
+    Sides[Sides["Left"] = 1] = "Left";
+    Sides[Sides["Right"] = 2] = "Right";
+    Sides[Sides["Up"] = 10] = "Up";
+    Sides[Sides["Down"] = 11] = "Down";
+})(Sides || (Sides = {}));
+let first = {
+    name: "Oskar",
+    age: 24
+};
+let second = ["21", 21];
+let third = [18, 20, 50, 1239];
+let fifth = {
+    model: "Mercedes",
+    year: 2020
+};
+second.push("abc");
+third.push(34325);
+console.log(Sides.Up + Sides.Right);
+let Mercedes = 2020;
+let error = 404;
+let secondPerson = {
+    name: "Oskar",
+    age: 24,
+    isActive: true
+};
+const addNumbers = (a, b = 10) => {
+    return a + b;
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-console.log(typeof 12354654);
+enum Sides {
+    Left = 1,
+    Right,
+    Up = 10,
+    Down
+}
+interface Person {
+    name: string,
+    surrname?: string,
+    age: number
+} // only use to objects
+interface Active extends Person {
+    isActive: boolean
+} 
+type CarYear = number; // one line type defined
+type Car = {
+    model: string,
+    year: number
+}
+
+
+let first: Person = {
+    name: "Oskar",
+    age: 24
+}
+let second: [string, number] = ["21", 21];
+let third: number[] = [18,20,50,1239];
+let fifth: Car = {
+    model: "Mercedes",
+    year: 2020
+}
+second.push("abc"); // bad reason for tuples, it can only contain two items
+third.push(34325);
+console.log(Sides.Up + Sides.Right); //Reuse enum values
+let Mercedes: CarYear = 2020; // only can contain number because type CarYear have number 
+
+let error: number | string = 404; // it's can contain number or string, nothing more
+let secondPerson: Active = {
+    name: "Oskar",
+    age: 24,
+    isActive: true
+} // Person interface with additional criterion isActive
+const addNumbers = (a: number, b: number = 10) => {
+    return a + b; // Only can use number functionalities
+}
